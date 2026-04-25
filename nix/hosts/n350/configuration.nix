9bbdec0f23e340a04f6dfeea5ecff3af2d64286e
Schokopuddingg <meow@schokopuddingg.de>
2026-04-25 22:39:42 +0200

    added config for n350

--- nix/hosts/n350/configuration.nix
+++ nix/hosts/n350/configuration.nix
@@ -5,13 +5,16 @@
     ./hardware-configuration.nix
     flake.modules.nixos.common
     flake.modules.nixos.graphical
+    flake.modules.nixos.wireless
+    flake.modules.nixos.gaming
+    flake.modules.nixos.wine
+    flake.modules.nixos.gc
+    flake.modules.nixos.tailscale
   ];
-
-  # TODO: replace with generated config!!!
 
   # Enable networking
   networking.networkmanager.enable = true;
 
-  system.stateVersion = "25.11"; # Did you read the comment?
+  system.stateVersion = "25.05"; # Did you read the comment?
 
 }
