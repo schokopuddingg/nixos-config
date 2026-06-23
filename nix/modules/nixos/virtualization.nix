e0e0eec165411a966db9b2b0fc65c831f3ba506a
Schokopuddingg <meow@schokopuddingg.de>
2026-06-23 10:53:04 +0200

    added docker and config for winboat

--- nix/modules/nixos/virtualization.nix
+++ nix/modules/nixos/virtualization.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }:
 {
+  virtualisation.docker.enable = true;
+
+  users.users.schokopuddingg.extraGroups = [ "docker" ];   # temp; need to find out how/where to put that line so that I don't have to have "schokopuddingg" standing there...
+
   environment.systemPackages = [
     pkgs.winboat
   ];
